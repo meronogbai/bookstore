84c8838442b0799111471886f3b6822d23c25b9d
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 15:33:17 +0300

    Setup books list and connect to redux store

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,19 @@
-const Book = book => (
-  <tr key={book.id}>
+import PropTypes from 'prop-types';
+
+const Book = ({ book }) => (
+  <tr>
     <td>{book.id}</td>
     <td>{book.title}</td>
     <td>{book.category}</td>
   </tr>
 );
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,11 +1,26 @@
-const BooksList = () => (
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => (
   <table>
     <tr>
       <th>ID</th>
       <th>Title</th>
       <th>Category</th>
     </tr>
+    {books.map(book => (
+      <Book book={book} key={book.id} />
+    ))}
   </table>
 );
+const mapStateToProps = state => ({ books: state });
 
-export default BooksList;
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  })).isRequired,
+};
+export default connect(mapStateToProps)(BooksList);
