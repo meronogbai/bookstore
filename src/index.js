a8998638dfa8663b010f90b21cdf17316be84353
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 14:07:42 +0300

    Create store and setup initial state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
 import App from './components/App';
+import booksReducer from './reducers/books';
 
+const initialState = [
+  {
+    id: Math.random() * 10,
+    title: 'The Hitchhiker\'s Guide to The Galaxy',
+    category: 'Sci-Fi',
+  },
+  {
+    id: Math.random() * 10,
+    title: 'Frankenstein',
+    category: 'Horror',
+  },
+  {
+    id: Math.random() * 10,
+    title: 'Algorithms Textbook',
+    category: 'Learning',
+  },
+];
+const store = createStore(booksReducer, initialState);
 ReactDOM.render(
   <React.StrictMode>
     <App />
