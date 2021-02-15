470794ab778602b0600ab74cf6c877ff0c804a04
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 15:32:51 +0300

    Setup redux store

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/App';
 import booksReducer from './reducers/books';
 
 const initialState = [
   {
-    id: Math.random() * 10,
+    id: Math.floor(Math.random() * 10),
     title: 'The Hitchhiker\'s Guide to The Galaxy',
     category: 'Sci-Fi',
   },
   {
-    id: Math.random() * 10,
+    id: Math.floor(Math.random() * 10),
     title: 'Frankenstein',
     category: 'Horror',
   },
   {
-    id: Math.random() * 10,
+    id: Math.floor(Math.random() * 10),
     title: 'Algorithms Textbook',
     category: 'Learning',
   },
@@ -25,7 +26,9 @@ const initialState = [
 const store = createStore(booksReducer, initialState);
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
