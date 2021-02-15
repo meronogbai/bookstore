689a03a6719f86708e54c97abc20828114f3d987
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 18:26:10 +0300

    Make books form stateful and handle changes

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+
 const BooksForm = () => {
   const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('Learning');
+  const handleTitleChange = e => setTitle(e.target.value);
+  const handleCategoryChange = e => setCategory(e.target.value);
   return (
     <form>
       <label htmlFor="title">
         Title
-        <input type="text" id="title" />
+        <input type="text" id="title" value={title} onChange={handleTitleChange} />
       </label>
-      <select name="category" id="category">
+      <select name="category" id="category" value={category} onChange={handleCategoryChange}>
         {categories.map((category, index) => (
           <option value={category} key={String(index)}>{category}</option>
         ))}
