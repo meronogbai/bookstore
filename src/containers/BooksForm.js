1a3294e304bd36e7de29c214b22451a277804e72
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 11:24:18 +0300

    Implement Books Form

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,18 @@
+const BooksForm = () => {
+  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+  return (
+    <form>
+      <label htmlFor="title">
+        Surname
+        <input type="text" id="title" />
+      </label>
+      <select name="category" id="category">
+        {categories.map((category, index) => (
+          <option value={category} key={String(index)}>{category}</option>
+        ))}
+      </select>
+    </form>
+  );
+};
+
+export default BooksForm;
