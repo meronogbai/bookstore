c428559acd4e1bce8c1bfcd94a9828abab6a8259
Meron Ogbai <okbaymeron@gmail.com>
2021-02-15 18:12:03 +0300

    Move actions creators to actions/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,6 @@
 import { combineReducers } from 'redux';
 import booksReducer from './books';
 
-export const createBook = book => ({
-  type: 'CREATE_BOOK',
-  book,
-});
-
-export const removeBook = book => ({
-  type: 'REMOVE_BOOK',
-  book,
-});
-
 const reducer = combineReducers({
   books: booksReducer,
 });
