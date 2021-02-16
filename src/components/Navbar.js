e6b036e6f6a1764ac2d4eb75b47fdaf90a7d33b9
Meron Ogbai <okbaymeron@gmail.com>
2021-02-16 09:43:17 +0300

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
-  <nav>
-    <h1>Bookstore CMS</h1>
-    <span>BOOKS</span>
-    <span>CATEGORIES</span>
-    <FontAwesomeIcon icon={faUser} />
+  <nav className="flex a-center jc-between">
+    <div className="flex a-center">
+      <h1 className="azure-text">Bookstore CMS</h1>
+      <span>BOOKS</span>
+      <span>CATEGORIES</span>
+    </div>
+    <div className="oval">
+      <FontAwesomeIcon icon={faUser} className="azure-text" />
+    </div>
   </nav>
 );
 
